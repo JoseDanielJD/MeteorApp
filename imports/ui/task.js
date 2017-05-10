8553c0145001e21aa3fdaa01988f703be05c7152
JoseDanielJD <jd24cm@gmail.com>
2017-05-10 01:22:18 +0000

    Todo list, cout, hide, delete

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -8,7 +8,7 @@ Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
     Tasks.update(this._id, {
-      $set: { x: ! this.x },
+      $set: { checked: ! this.checked },
     });
   },
   'click .delete'() {
